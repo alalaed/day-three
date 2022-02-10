f007c46be764a6ab256776a98ff44f28f05902b5
Ali Alaeddine <94364910+alalaed@users.noreply.github.com>
2022-02-10 18:23:40 +0100

    reviews fixed

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,40 +1,58 @@
 import { Component } from "react";
-import { Card, Form, ListGroup, Badge, Alert } from "react-bootstrap";
+import { Modal, Alert, Button, ListGroup, Badge } from "react-bootstrap";
 
-class Comment extends Component {
+class CommentArea extends Component {
+  componentDidMount = async () => {
+    let httpFetch =
+      "https://striveschool-api.herokuapp.com/api/comments/" + this.props.id;
+    try {
+      let response = await fetch(httpFetch, {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNjM5OTgyZWExZDAwMTViYjAzZTkiLCJpYXQiOjE2NDM3OTk0NTAsImV4cCI6MTY0NTAwOTA1MH0.5dZKYBo7eP4on-pcM8VC0B42JNHHlwPOAqk70FWRG1M",
+        },
+      });
+      if (response.ok) {
+        let data = await response.json();
+        this.setState({ ...this.state, previousComments: data });
+      } else {
+        <Alert variant="danger">OOPS!!!</Alert>;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  state = {
+    previousComments: [],
+  };
+
   render() {
     return (
-      <Form key={this.props.id}>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Control
-            type="text"
-            placeholder="comment"
-            value={this.state.comment}
-            onChange={(e) => this.setState({ comment: e.target.value })}
-          />
-          <Form.Control
-            type="text"
-            placeholder="rate"
-            value={this.state.rate}
-            onChange={(e) => this.setState({ rate: e.target.value })}
-          />
-        </Form.Group>
-        <ListGroup>
-          <h4>Previous Comments</h4>
-          <ListGroup.Item
-            as="li"
-            className="d-flex justify-content-between align-items-start"
-            key={this.props.asin}
-          >
-            <div className="ms-2 me-auto">Nice!!!</div>
-            <Badge variant="primary" pill>
-              14
-            </Badge>
-          </ListGroup.Item>
-        </ListGroup>
-      </Form>
+      <Modal.Dialog>
+        <Modal.Header closeButton>
+          <Modal.Title>Reviews</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <ListGroup>
+            {this.state.previousComments.map((comment) => (
+              <ListGroup.Item>
+                {comment.comment}
+                <Badge variant="primary" pill>
+                  {comment.rate}
+                </Badge>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary">Close</Button>
+          <Button variant="primary">Save changes</Button>
+        </Modal.Footer>
+      </Modal.Dialog>
     );
   }
 }
-
-export default Comment;
+export default CommentArea;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,55 +1,10 @@
 import { Component } from "react";
-import { Card, Form, ListGroup, Badge, Alert, Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import CommentArea from "./CommentArea";
 
 class SingleBook extends Component {
   state = {
     selected: false,
-    comment: "",
-    rate: "",
-    id: "",
-    previousComments: [],
-  };
-
-  componentDidMount = async () => {
-    let httpFetch =
-      "https://striveschool-api.herokuapp.com/api/comments/" + this.props.id;
-    try {
-      let response = await fetch(httpFetch, {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNjM5OTgyZWExZDAwMTViYjAzZTkiLCJpYXQiOjE2NDM3OTk0NTAsImV4cCI6MTY0NTAwOTA1MH0.5dZKYBo7eP4on-pcM8VC0B42JNHHlwPOAqk70FWRG1M",
-        },
-      });
-      if (response.ok) {
-        let data = await response.json();
-        this.setState({ ...this.state, previousComments: data });
-      } else {
-        <Alert variant="danger">OOPS!!!</Alert>;
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  submitComment = (e) => {
-    e.preventDefault();
-    let httpFetch =
-      "https://striveschool-api.herokuapp.com/api/comments/" + this.props.id;
-    fetch(httpFetch, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNjM5OTgyZWExZDAwMTViYjAzZTkiLCJpYXQiOjE2NDM3OTk0NTAsImV4cCI6MTY0NTAwOTA1MH0.5dZKYBo7eP4on-pcM8VC0B42JNHHlwPOAqk70FWRG1M",
-      },
-      body: JSON.stringify({
-        comment: this.state.comment,
-        rate: this.state.rate,
-        elementId: this.props.id,
-      }),
-    })
-      .then((response) => response.json)
-      .then((data) => console.log(data));
   };
 
   render() {
@@ -70,56 +25,8 @@ class SingleBook extends Component {
         />
         <Card.Body>
           <Card.Title>{this.props.title}</Card.Title>
-          {this.state.selected && (
-            <Form key={this.props.id}>
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlInput1"
-              >
-                <Form.Control
-                  type="text"
-                  placeholder="comment"
-                  value={this.state.comment}
-                  onChange={(e) =>
-                    this.setState({ ...this.state, comment: e.target.value })
-                  }
-                />
-                <Form.Control
-                  type="text"
-                  placeholder="rate"
-                  value={this.state.rate}
-                  onChange={(e) =>
-                    this.setState({ ...this.state, rate: e.target.value })
-                  }
-                />
-                <Button
-                  variant="primary"
-                  onClick={(e) => this.submitComment(e)}
-                >
-                  Submit
-                </Button>
-              </Form.Group>
-              <ListGroup as="ol">
-                <h4>Previous Comments</h4>
-
-                {this.state.previousComments.map((comment) => (
-                  <>
-                    <ListGroup.Item as="li" key={this.props.id}>
-                      {comment.comment}
-                      <Badge variant="primary" pill>
-                        {comment.rate}
-                      </Badge>
-                    </ListGroup.Item>
-                    {/* <div className="ms-2 me-auto">{comment.comment}</div>
-                      <Badge variant="primary" pill>
-                        {comment.rate}
-                      </Badge> */}
-                  </>
-                ))}
-              </ListGroup>
-            </Form>
-          )}
         </Card.Body>
+        {this.state.selected && <CommentArea id={this.props.id} />}
       </Card>
     );
   }
