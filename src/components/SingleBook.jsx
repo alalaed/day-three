fcf64a6fd9b098e1078936f9816d84ab7bad6eee
Ali Alaeddine <94364910+alalaed@users.noreply.github.com>
2022-02-10 14:20:16 +0100

    CRUD implemented

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,12 +1,37 @@
 import { Component } from "react";
-import { Card, Form } from "react-bootstrap";
+import { Card, Form, ListGroup, Badge, Alert } from "react-bootstrap";
 
 class SingleBook extends Component {
   state = {
     selected: false,
     comment: "",
     rate: "",
+    id: "",
     previousComments: [],
+  };
+
+  componentDidMount = async () => {
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments/",
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWZhNjM5OTgyZWExZDAwMTViYjAzZTkiLCJpYXQiOjE2NDM3OTk0NTAsImV4cCI6MTY0NTAwOTA1MH0.5dZKYBo7eP4on-pcM8VC0B42JNHHlwPOAqk70FWRG1M",
+          },
+        }
+      );
+      if (response.ok) {
+        let data = await response.json();
+        this.setState({
+          previousComments: data,
+        });
+      } else {
+        <Alert variant="danger">OOPS!!!</Alert>;
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
   render() {
     return (
@@ -43,6 +68,19 @@ class SingleBook extends Component {
                   onChange={(e) => this.setState({ rate: e.target.value })}
                 />
               </Form.Group>
+              <ListGroup>
+                <h4>Previous Comments</h4>
+                <ListGroup.Item
+                  as="li"
+                  className="d-flex justify-content-between align-items-start"
+                  key={this.props.asin}
+                >
+                  <div className="ms-2 me-auto">Nice!!!</div>
+                  <Badge variant="primary" pill>
+                    14
+                  </Badge>
+                </ListGroup.Item>
+              </ListGroup>
             </Form>
           )}
         </Card.Body>
