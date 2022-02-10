e6e487aeabc5cd0b50722b736992c98c9b6c25f7
Ali Alaeddine <94364910+alalaed@users.noreply.github.com>
2022-02-10 12:44:48 +0100

    corrected

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import WarningSign from "./components/WarningSign";
 import MyBadge from "./MyBadge";
 import books from "../src/data/horror.json";
-import SingleBook2 from "./components/SingleBook";
+// import SingleBook from "./components/SingleBook";
 import BookList from "./components/BookList";
 const book = books[Math.floor(Math.random() * books.length)];
 
@@ -14,9 +14,9 @@ function App() {
     <div className="App">
       <WarningSign string="Ali" />
       <MyBadge text="hi Ali" color="primary" />
-      <SingleBook2 title={title} image={img} />
+      {/* <SingleBook title={title} image={img} /> */}
 
-      <BookList category={books} />
+      <BookList array={books} />
     </div>
   );
 }
