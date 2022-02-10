486530dc9bbf72c7fddcb94d78300556e5cd1be6
Ali Alaeddine <94364910+alalaed@users.noreply.github.com>
2022-02-10 12:47:10 +0100

    saved

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import WarningSign from "./components/WarningSign";
-import MyBadge from "./MyBadge";
+// import WarningSign from "./components/WarningSign";
+// import MyBadge from "./MyBadge";
 import books from "../src/data/horror.json";
 // import SingleBook from "./components/SingleBook";
 import BookList from "./components/BookList";
-const book = books[Math.floor(Math.random() * books.length)];
-
-const { img, title } = book;
 
 function App() {
   return (
     <div className="App">
-      <WarningSign string="Ali" />
-      <MyBadge text="hi Ali" color="primary" />
-      {/* <SingleBook title={title} image={img} /> */}
+      {/* <WarningSign string="Ali" />
+      <MyBadge text="hi Ali" color="primary" /> */}
 
       <BookList array={books} />
     </div>
